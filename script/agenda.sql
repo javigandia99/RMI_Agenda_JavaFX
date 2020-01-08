98fcfd84f5ad24039f71da08b3aea4aad005b74f
javier <javigandia99@gmail.com>
2020-01-08 14:25:37 +0100

    Added script database (SQL) with sample data

--- script/agenda.sql
+++ script/agenda.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-01-2020 a las 20:49:15
+-- Tiempo de generación: 05-01-2020 a las 16:25:10
 -- Versión del servidor: 10.1.38-MariaDB
 -- Versión de PHP: 7.3.2
 
@@ -29,44 +29,65 @@ USE `agenda`;
 --
 -- Estructura de tabla para la tabla `contacts`
 --
+-- Creación: 04-01-2020 a las 18:06:38
+--
 
 DROP TABLE IF EXISTS `contacts`;
-CREATE TABLE `contacts` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `contacts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
   `surname` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
   `telephone` int(16) NOT NULL,
   `movil` int(16) NOT NULL,
-  `ref_user` varchar(25) COLLATE utf8_spanish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+  `ref_user` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- RELACIONES PARA LA TABLA `contacts`:
+--   `ref_user`
+--       `users` -> `username`
+--
 
 --
 -- Volcado de datos para la tabla `contacts`
 --
 
 INSERT INTO `contacts` (`id`, `name`, `surname`, `telephone`, `movil`, `ref_user`) VALUES
-(1, 'Javier', 'admin', 916704567, 678905436, ''),
-(4, 'Marta', 'Velazquez', 987654321, 635678321, ''),
-(5, 'Pedro', 'Sanchez', 980765234, 654789098, ''),
-(6, 'ty', 'nuevo', 123456789, 987654321, ''),
-(7, 'wert', 'wertyu', 12349876, 6543222, ''),
-(8, 'Pablo', 'Motos', 12345432, 654321987, ''),
 (9, 'Susana', 'Diaz', 654122345, 876987098, 'w'),
-(10, 'Jaime', 'Casado', 54321678, 916709832, '1234');
+(10, 'Jaime', 'Casado', 54321678, 916709832, '1234'),
+(11, 'we', 'we', 7654, 321098, 'w'),
+(12, 'Prueba', 'final', 90876543, 654321789, 'w'),
+(13, 'prueba', 'de javi', 987654321, 123456789, 'javi'),
+(14, 'prueba', 'de qwerty', 987654321, 123456776, 'qwerty'),
+(15, 'de qwerty', 'prueba 2', 98765432, 67890543, 'qwerty'),
+(16, 'movil no ', 'cambiar (igual)', 123456789, 4321, 'w'),
+(19, 'prueba', 'de xx', 654321789, 914356778, 'xx'),
+(20, 'movil clave', 'no func si change', 987654324, 646122321, 'w'),
+(21, 'movil clave', 'movil fijo', 987650987, 656433212, 'w'),
+(25, 'Marta', 'Velazquez', 915678745, 632123545, 'Pedro1234'),
+(26, 'Pedro', 'Camacho Ortega', 917894321, 676544578, 'Pedro1234');
 
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `users`
 --
+-- Creación: 17-12-2019 a las 11:46:23
+--
 
 DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
+CREATE TABLE IF NOT EXISTS `users` (
   `username` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
   `password` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
   `name` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
-  `surname` varchar(25) COLLATE utf8_spanish_ci NOT NULL
+  `surname` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- RELACIONES PARA LA TABLA `users`:
+--
 
 --
 -- Volcado de datos para la tabla `users`
@@ -74,35 +95,10 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`username`, `password`, `name`, `surname`) VALUES
 ('1234', '1234', 'Prueba ', '1'),
-('Pedro1234', '1234', 'Pedro', 'Camacho Ortega'),
+('javi', '54321', 'Javi', 'Gandia'),
+('Pedro1234', 'pedro1234', 'Pedro', 'Camacho Ortega'),
 ('qwerty', 'qwerty', 'qwerty', 'qwerty'),
 ('w', 'w', 'Prueba', '2');
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `contacts`
---
-ALTER TABLE `contacts`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`username`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `contacts`
---
-ALTER TABLE `contacts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
